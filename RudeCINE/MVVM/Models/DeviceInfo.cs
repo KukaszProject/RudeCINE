53a85a0cc46a3e0f96daaf32c03d1e639da6b202
Łukasz Kiełbasa <hamsingming@gmail.com>
2024-11-21 21:03:12 +0100

    Dodaj pliki projektów.

---
+++ RudeCINE/MVVM/Models/DeviceInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudeCINE.MVVM.Models
+{
+    internal class DeviceInfo : IDeviceDisplay
+    {
+        public bool KeepScreenOn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public DisplayInfo MainDisplayInfo => throw new NotImplementedException();
+        public void ReadDeviceDisplay()
+        {
+            var width = DeviceDisplay.Current.MainDisplayInfo.Width;
+            var height = DeviceDisplay.Current.MainDisplayInfo.Height;
+        }
+
+        public event EventHandler<DisplayInfoChangedEventArgs> MainDisplayInfoChanged;
+    }
+}
